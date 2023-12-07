54ba71be96586fa8db4193ff4ba348de8ee459b2
Gaurav Saraswat <96509978+gaurav10000@users.noreply.github.com>
2023-12-07 13:07:42 +0000

    conversion of dataTypes

---
+++ Basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+const score = null
+
+console.log(typeof score);
+
+
+
+/* conversion to number
+"33" => 33
+"33sadd" => NaN
+true => 1 and false => 0
+null => 0
+*/
+
+/* conversion to boolean
+"" => false
+"something" => true
+*/
+
+/*
+33 => "33"
+
+*/
