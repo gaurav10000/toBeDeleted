2f509aafb447c2f5a1cfab588b647b5943923fe5
Gaurav Saraswat <96509978+gaurav10000@users.noreply.github.com>
2023-12-07 17:27:26 +0000

    new files

---
+++ 01_arrays.js
@@ -0,0 +1,21 @@
+// arrays
+
+
+const arr = [1,2,3,4,5,6]
+
+// console.log(arr.push(7));
+// arr.unshift([0,9])
+// arr.shift()
+// console.log(arr);
+
+// console.log(typeof arr.join());
+
+
+//  slice and splice
+
+// console.log(arr.slice(1,3)); 
+// console.log(arr); // we can conclude that while slicing it return a copy of og from defined starting point to defined second last point (i.e last element is not included) and og array does not change
+
+// console.log(arr.splice(1,3));
+// console.log(arr); // Removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements. i.e cuts the defined section (including both staring and ending point) from og array.
+
---
+++ Basics/07_datesInJs.js
@@ -0,0 +1,22 @@
+
+
+// const myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toLocaleString("en-In"));
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString()); // 12/7/2023
+
+//     let myCreatedDate = new Date(2023, 1, 23, 5, 3, 23)
+
+//     // let myTimeStamp = Date.now()
+//     // console.log(myTimeStamp);
+//     // console.log(myCreatedDate.getTime());
+
+// const week = myCreatedDate.toLocaleString('default', {
+//     dateStyle: "long"
+// })
+
+// console.log(week);
+
