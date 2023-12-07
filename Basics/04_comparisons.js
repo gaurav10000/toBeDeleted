dcb4329cb8c682ee1cf9b345dd20472b586608e9
Gaurav Saraswat <96509978+gaurav10000@users.noreply.github.com>
2023-12-07 13:59:48 +0000

    explored some more datatypes in comparisons file

--- Basics/04_comparisons.js
+++ Basics/04_comparisons.js
@@ -7,6 +7,29 @@
 // console.log(undefined > 0); // false
 // console.log(undefined < 0); // false
 
-// arr = [1,2,3,4]
+// const arr = ["1","2","3",4]
 
-// console.log(arr);
+// console.log(typeof arr);
+
+
+
+// const id = Symbol('abc')
+// const anotherId = Symbol('abc')
+
+// console.log(id);
+// console.log(anotherId);
+
+// console.log(id === anotherId);
+// console.log(id == anotherId);
+
+
+// const bigNumber = 198373285963295638956385n // just by adding n at last we can declare them bigInt
+
+// console.log(typeof bigNumber);
+
+
+// const fun =  () => {
+//     console.log(typeof fun);
+// }
+
+// fun()
