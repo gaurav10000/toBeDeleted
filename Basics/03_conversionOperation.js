a1287766e0a26fc5d56ae53826904929d2aa173f
Gaurav Saraswat <96509978+gaurav10000@users.noreply.github.com>
2023-12-07 13:47:33 +0000

    checking some comparisons

--- Basics/03_conversionOperation.js
+++ Basics/03_conversionOperation.js
@@ -1,6 +1,6 @@
 const score = null
 
-console.log(typeof score);
+// console.log(typeof score);
 
 
 
@@ -20,3 +20,13 @@ null => 0
 33 => "33"
 
 */
+
+// let str1 = "Gaurav"
+// let str2 = "Saraswat"
+
+// let str3 = str1.concat(str2)
+
+// console.log(str3);
+
+// console.log(+true); // => 1
+// console.log(+""); // => 0
---
+++ Basics/04_comparisons.js
@@ -0,0 +1,12 @@
+// in js problem comes when we compare two values with different dataTypes, many times result is not predictable
+
+// console.log(null == 0); // false
+// console.log(null === 0); // false // this is strict check and checks for dataTypes as well
+
+// console.log(undefined == 0); // false
+// console.log(undefined > 0); // false
+// console.log(undefined < 0); // false
+
+// arr = [1,2,3,4]
+
+// console.log(arr);
