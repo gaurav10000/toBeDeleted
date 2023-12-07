9248ceaffe1f21bbde7a54fd85a81c7cb0bb670a
Gaurav Saraswat <96509978+gaurav10000@users.noreply.github.com>
2023-12-07 16:33:34 +0000

    maths and numers and strings

---
+++ Basics/05_strings.js
@@ -0,0 +1,5 @@
+const str1 = "   dfdasf    "
+// console.log(str1.trimStart().length);
+// console.log(str1.replace("dfd", "gaurav"));
+// console.log(str1.includes("   "));
+// console.log(str1.blink());
---
+++ Basics/06_Numbers-Maths.js
@@ -0,0 +1,9 @@
+// const num1 = 12412441
+// // console.log(Number.isFinite(num1));
+// console.log(num1.toLocaleString("en-IN"));
+
+// one of the method to find a random number in a range
+const max = 50
+const min = 40
+const randNumber = Math.floor(Math.random() * (max - min + 1)) + min
+console.log(randNumber);
